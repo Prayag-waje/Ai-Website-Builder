98005201029d60aacc7cf12ee8a8f600ad7b0ccd
Prayag-waje <prayagwaje21@gmail.com>
2026-05-09 01:52:09 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ import cors from "cors";
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
-import cors from "cors"
-app.use(cors());
 import userRouter from "./routes/user.routes.js"
 import websiteRouter from "./routes/website.routes.js"
 import billingRouter from "./routes/billing.routes.js"
