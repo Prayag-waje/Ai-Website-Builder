35e530db03ae9ca2c65845f66c889f2343677718
Prayag-waje <prayagwaje21@gmail.com>
2026-05-09 01:08:42 +0530

    localhost  added

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"http://3.89.189.112:5000",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
